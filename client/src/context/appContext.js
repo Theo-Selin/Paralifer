ba6aefa6abe4ac90680fe85537194cbe51274339
Theo Selin <theo.selin@hotmail.com>
2022-04-11 17:12:35 +0200

    axios register post request

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -1,6 +1,7 @@
 import React, {useReducer, useContext} from 'react'
 
 import reducer from './reducer'
+import axios from 'axios'
 
 import { 
     DISPLAY_ALERT, 
@@ -17,7 +18,8 @@ const initialState = {
     alertType: "",
     user: null,
     token: null,
-    userLocation: ""
+    userLocation: "",
+    jobLocation: ""
 }
 
 const AppContext = React.createContext()
@@ -38,7 +40,24 @@ const AppProvider = ({children}) => {
     }
 
     const registerUser = async (currentUser) => {
-        console.log(currentUser)
+        dispatch({ type: REGISTER_USER_BEGIN })
+        try {
+            const response = await axios.post("/api/v1/auth/register", currentUser)
+            console.log(response)
+            const {user, token, location} = response.data
+            dispatch({
+                type: REGISTER_USER_SUCCESS, 
+                payload: {user, token, location}
+            })
+            // local storage implementation
+        } catch (error) {
+            console.log(error.response)
+            dispatch({
+                type: REGISTER_USER_ERROR, 
+                payload: { msg: error.response.data.message }
+            })
+        }
+        clearAlert()
     }
 
     return <AppContext.Provider value={{...state, displayAlert, registerUser}}>
